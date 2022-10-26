70bcd8e00cd0c02d251d8b948c2a55bfaa37bec1
endyjams <112718430+endyjams@users.noreply.github.com>
2022-10-26 17:42:16 -0300

    commit1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rb;
+
+    [SerializeField] private float _strength = 10f;
+
+    private void Awake() 
+    {
+        _rb = this.GetComponent<Rigidbody2D>();    
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        _rb.AddForce(Vector2.up * _strength, ForceMode2D.Impulse);
+    }
+}
